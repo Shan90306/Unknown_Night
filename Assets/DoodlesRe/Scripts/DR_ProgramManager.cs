c988ff7c1cee8de797f4c6a81e6475fe47711cd2
LeeSeungYeop-KR <73506744+LeeSeungYeop-KR@users.noreply.github.com>
2021-04-28 15:25:22 +0900

    - 프로젝트 첫 설정
    Dotween 추가
    인트로 씬 추가
     프로그램 매니져 스크립트 추가

---
+++ Assets/DoodlesRe/Scripts/DR_ProgramManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoodlesRe
+{
+    /// <summary>
+    /// <para> 작 성 자 : 이승엽 </para>
+    /// <para> 작 성 일 : 2021-04-28 </para>
+    /// <para> 내    용 : 프로그램을 관리하는 매니져 클래스 </para>
+    /// </summary>
+    public class DR_ProgramManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+    }
+}
