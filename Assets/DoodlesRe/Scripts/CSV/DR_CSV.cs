9acbc15c02d2f25c4514b90b1fc5a70fcf28c4be
LeeSeungYeop-KR <73506744+LeeSeungYeop-KR@users.noreply.github.com>
2021-05-21 18:34:17 +0900

    - Csv파일 스트림리더로 읽는 기능 개발 중

--- Assets/DoodlesRe/Scripts/CSV/DR_CSV.cs
+++ Assets/DoodlesRe/Scripts/CSV/DR_CSV.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text.RegularExpressions;
 using UnityEngine;
 
@@ -19,53 +20,58 @@ namespace DoodlesRe
         public static List<Dictionary<string, object>> Func_Read(string _file)
         {
             string _path = Application.dataPath + DR_PathDefine.CSV_SavePath + _file;
-
             var _list = new List<Dictionary<string, object>>();
-            TextAsset _data = Resources.Load(_file) as TextAsset;
+            FileInfo _fileInfo = new FileInfo(_path);
 
-            var _line = Regex.Split(_data.text, LINE_SPLIT);
-            if (_line.Length <= 1)
+            if (_fileInfo.Exists)
             {
+                StreamReader _sr = new StreamReader(_path)
+                TextAsset _data = Resources.Load(_file) as TextAsset;
+
+                var _line = Regex.Split(_data.text, LINE_SPLIT);
+                if (_line.Length <= 1)
+                {
+                    return _list;
+                }
+
+                var _header = Regex.Split(_line[0], SPLIT);
+                for (int i = 1; i < _line.Length; i++)
+                {
+                    var _values = Regex.Split(_line[i], SPLIT);
+                    if (_values.Length == 0 || _values[0] == "")
+                    {
+                        continue;
+                    }
+
+                    var _entry = new Dictionary<string, object>();
+                    for (int j = 0; j < _header.Length && j < _values.Length; j++)
+                    {
+                        string _value = _values[j];
+                        _value = _value.TrimStart(TRIM_CHAR_ARR).TrimEnd(TRIM_CHAR_ARR).Replace("\\", "");
+                        _value = _value.Replace("<br>", "\n");
+                        _value = _value.Replace("<c>", ",");
+
+                        object _finalValue = _value;
+                        int _n;
+                        float _f;
+
+                        if (int.TryParse(_value, out _n))
+                        {
+                            _finalValue = _n;
+                        }
+                        else if (float.TryParse(_value, out _f))
+                        {
+                            _finalValue = _f;
+                        }
+
+                        _entry[_header[j]] = _finalValue;
+                    }
+
+                    _list.Add(_entry);
+                }
+
                 return _list;
             }
-
-            var _header = Regex.Split(_line[0], SPLIT);
-            for (int i = 1; i < _line.Length; i++)
-            {
-                var _values = Regex.Split(_line[i], SPLIT);
-                if (_values.Length == 0 || _values[0] == "")
-                {
-                    continue;
-                }
-
-                var _entry = new Dictionary<string, object>();
-                for (int j = 0; j < _header.Length && j < _values.Length; j++)
-                {
-                    string _value = _values[j];
-                    _value = _value.TrimStart(TRIM_CHAR_ARR).TrimEnd(TRIM_CHAR_ARR).Replace("\\", "");
-                    _value = _value.Replace("<br>", "\n");
-                    _value = _value.Replace("<c>", ",");
-
-                    object _finalValue = _value;
-                    int _n;
-                    float _f;
-
-                    if (int.TryParse(_value, out _n))
-                    {
-                        _finalValue = _n;
-                    }
-                    else if (float.TryParse(_value, out _f))
-                    {
-                        _finalValue = _f;
-                    }
-
-                    _entry[_header[j]] = _finalValue;
-                }
-
-                _list.Add(_entry);
-            }
-
-            return _list;
         }
     }
 }
--- Assets/DoodlesRe/Scripts/Test/DR_Test.cs
+++ Assets/DoodlesRe/Scripts/Test/DR_Test.cs
@@ -13,51 +13,13 @@ namespace DoodlesRe
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                string _path = Application.dataPath + DR_PathDefine.ScreenShot_SavePathName;
-                System.IO.DirectoryInfo directoryInfo = new System.IO.DirectoryInfo(_path);
-                if (directoryInfo.Exists)
-                {
-                    _path = Application.dataPath + DR_PathDefine.ScreenShot_SavePath + DR_PathDefine.XML_SaveName + 0 + ".png";
-                    RenderTexture _renderTexture = new RenderTexture(Screen.width, Screen.height, 24);
-                    Texture2D _texture2D = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-                    Rect _rect = new Rect(0, 0, Screen.width, Screen.height);
+               
 
-                    Camera _mainCam = Camera.main;
-                    _mainCam.targetTexture = _renderTexture;
-                    _mainCam.Render();
-                    RenderTexture.active = _renderTexture;
-
-                    _texture2D.ReadPixels(_rect, 0, 0);
-                    _texture2D.Apply();
-
-                    byte[] _byteArr = _texture2D.EncodeToPNG();
-                    System.IO.File.WriteAllBytes(_path, _byteArr);
-
-                    RenderTexture.active = null;
-                    _mainCam.targetTexture = null;
-                    Destroy(_renderTexture);
-                    Debug.Log("스크린샷 생성");
-                }
-                else
-                {
-                    directoryInfo.Create();
-                    Debug.Log("스크린샷 폴더 생성");
-                }
             }
 
             if (Input.GetKeyDown(KeyCode.A))
             {
-                string _path = Application.dataPath + DR_PathDefine.ScreenShot_SavePath + DR_PathDefine.XML_SaveName + 0 + ".png";
-                System.IO.FileInfo fileInfo = new System.IO.FileInfo(_path);
-                if (fileInfo.Exists)
-                {
-                    Debug.Log("스크린샷 있음");
-                }
-                else
-                {
-                    Debug.Log("스크린샷 없음");
-
-                }
+               
             }
         }
     }
