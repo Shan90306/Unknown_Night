d6270e3283865c9430c3974c8c5238db0538a0a2
LeeSeungYeop-KR <73506744+LeeSeungYeop-KR@users.noreply.github.com>
2021-05-31 22:10:01 +0900

    - 메인 월드맵 메뉴 추가 중

---
+++ Assets/DoodlesRe/Scripts/UI/DR_ButtonArea.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DoodlesRe
+{
+    /// <summary>
+    /// <para> 작 성 자 : 이승엽 </para>
+    /// <para> 작 성 일 : 2021-05-31 </para>
+    /// <para> 내    용 : 지역 버튼 클래스 </para>
+    /// </summary>
+    public class DR_ButtonArea : MonoBehaviour
+    {
+        [SerializeField] private DR_ButtonAreaMenu buttonMenu;
+
+        [Header("- 버튼 이미지 배열")]
+        [SerializeField] private Sprite[] spriteArr;    // 0: 비활성화  1: 활성화
+
+        [Header("- 버튼 이미지 배열")]
+        [SerializeField] private Image buttonImage;    // 0: 비활성화  1: 활성화
+
+        private bool isClick;
+
+        #region 기능
+
+        /// <summary>
+        /// <para> 작 성 자 : 이승엽 </para>
+        /// <para> 작 성 일 : 2021-05-31 </para>
+        /// <para> 내    용 : 버튼을 초기화하는 기능 </para>
+        /// </summary>
+        public void Func_Init()
+        {
+            isClick = false;
+            buttonImage.sprite = spriteArr[1];
+        }
+
+        #endregion
+
+        #region 버튼 메서드
+
+        /// <summary>
+        /// <para> 작 성 자 : 이승엽 </para>
+        /// <para> 작 성 일 : 2021-05-31 </para>
+        /// <para> 내    용 : 버튼을 눌렀을 때 호출되는 버튼 메서드 </para>
+        /// </summary>
+        public void Button_ClickArea()
+        {
+            buttonMenu.Func_AllDisable();
+
+
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/DoodlesRe/Scripts/UI/DR_ButtonAreaMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DoodlesRe
+{
+    /// <summary>
+    /// <para> 작 성 자 : 이승엽 </para>
+    /// <para> 작 성 일 : 2021-05-31 </para>
+    /// <para> 내    용 : 지역 버튼 메뉴 클래스 </para>
+    /// </summary>
+    public class DR_ButtonAreaMenu : MonoBehaviour
+    {
+        [Header("- 버튼 배열")]
+        [SerializeField] private DR_ButtonArea[] buttonArr;
+
+        public void Func_AllDisable()
+        {
+            for (int i = 0; i < buttonArr.Length; i++)
+            {
+                buttonArr[i].Func_Init();
+            }
+        }
+    }
+}
